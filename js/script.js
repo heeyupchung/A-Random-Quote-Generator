9c1f58d8a53657bb6b56774c8ab78e42bda16802
heeyupchung <heeyupchung@gmail.com>
2021-09-20 10:54:52 -0700

    Created randomBgColor() and setInterval

--- js/script.js
+++ js/script.js
@@ -15,33 +15,33 @@ let quotes = [
     quote: 'Carpe diem. Seize the day, boys. Make your lives extraordinary.',
     source: 'Robin Williams',
     citation: 'Dead Poets Society',
-    movie: 'movie',
+    medium: 'movie',
     year: 1989
   },
   {
     quote: 'It is not our abilities that show what we truly are... it is our choices.',
     source: 'Albus Dumbledore',
     citation: 'Harry Potter and the Chamber of Secrets',
-    book: 'book',
+    medium: 'book',
     year: 1998
   },
   {
     quote: "Home isn't where you land; home is where you launch. You can't pick your home any more than you can choose your family.",
     source: 'Tayari Jones',
     citation: 'An American Marriage',
-    book: 'book'
+    medium: 'book'
   },
   {
     quote: "To choose doubt as a philosophy of life is akin to choosing immobility as a means of transportation.",
     source: 'Yann Martel',
     citation: 'Life of Pi',
-    book: 'book'
+    medium: 'book'
   },
   {
     quote: "Enthusiasm makes up for a host of deficiencies.",
     source: 'Barack Obama',
     citation: 'A Promised Land',
-    book: 'book'
+    medium: 'book'
   }
 ];
 
@@ -66,12 +66,8 @@ function printQuote() {
     html += `<span class="citation"> ${randomQuote.citation}</span>`;
   }
 
-  if (randomQuote.book) {
-    html += `<span class="book">, ${randomQuote.book}</span>`;
-  }
-
-  if (randomQuote.movie) {
-    html += `<span class="movie">, ${randomQuote.movie}</span>`;
+  if (randomQuote.medium) {
+    html += `<span class="medium">, ${randomQuote.medium}</span>`;
   }
 
   if (randomQuote.year) {
@@ -81,15 +77,28 @@ function printQuote() {
   
 
   html += `</p>`;
+  randomBgColor();
   document.getElementById('quote-box').innerHTML = html; 
   return html;
 }
 
-
+// took a look at https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php for help
+function randomBgColor() {
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+  let bgColor = `rgb(${x} , ${y}, ${z})`;
+  document.body.style.backgroundColor = bgColor;
+}
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
+
+//prints a new random quote every 10 seconds
+setInterval(printQuote, 10000);
+
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
